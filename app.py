5452582005fbecf301eeb1398e6043bf4e7d83a7
MHR-u <coordinates2016@gmail.com>
2025-03-13 18:54:47 +0300

    app.py

--- app.py
+++ app.py
@@ -48,10 +48,10 @@ st.write("""
 - **a**: Shear span in (mm).
 - **d**: Effective depth in (mm).
 - **a/d**: Shear span-to-effective depth ratio (dimensionless).
-- **f'_c**: Concrete compressive strength in (MPa).
+- **f&#8242;c**: Concrete compressive strength in (MPa).
 - **ρ&#8290;t**: FRP longitudinal reinforcement ratio (input as percentage, e.g., 2 for 2%).
-- **f&#8242;c**: FRP ultimate tensile strength in (MPa).
-- **E&#8242;c**: FRP Young's modulus in (GPa).
+- **f**: FRP ultimate tensile strength in (MPa).
+- **E**: FRP Young's modulus in (GPa).
   
 ### Output:
 - **Vn**: Nominal shear strength of the section in kilo-Newtons (kN), calculated based on the provided parameters.
