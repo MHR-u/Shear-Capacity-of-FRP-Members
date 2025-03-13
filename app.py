2a5252f0acb11aeda0b49a9f0550904d996429aa
MHR-u <coordinates2016@gmail.com>
2025-03-13 21:10:34 +0300

    app.py

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ def calculate_vn(b, d, a, a_d, fc, ρt, fƒ, Eƒ):
     return Vn
 
 # Streamlit app interface
-st.set_page_config(page_title="Nominal Shear Strength (Vn) Calculator", page_icon="⚙️", layout="centered")
-st.title("Nominal Shear Strength (Vn) Calculator")
+st.set_page_config(page_title="Nominal Shear Strength (Vn) Calculator", page_icon="https://ars.els-cdn.com/content/image/1-s2.0-S2352012424006726-gr1.jpg", layout="centered")
+st.title(" page_icon Nominal Shear Strength (Vn) Calculator")
 
 # Definitions
 st.write(""" 
