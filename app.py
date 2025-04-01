84bde7570fa6edaaa831745ed7690623e5124312
MHR-u <coordinates2016@gmail.com>
2025-04-02 00:21:21 +0300

    app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def calculate_vn(b, d, a, a_d, fc, ρt, fƒ, Eƒ):
 # Streamlit app interface
 st.set_page_config(
     page_title="Shear Capacity of FRP-RC Members (Vn) Calculator",
-    page_icon="https://github.com/MHR-u/Shear-Capacity-of-FRP-Members/blob/main/FRP_RC.png",
+    page_icon="https://github.com/MHR-u/Shear-Capacity-of-FRP-Members/blob/main/FRP_RC.png?raw=true",
     layout="centered"
 )
 
@@ -97,7 +97,7 @@ st.markdown(f"""
     <div class="title-container">
         <div class="title-text">Shear Capacity of FRP-RC Members (Vn)</div>
         <div class="image-container">
-            <img src="https://github.com/MHR-u/Shear-Capacity-of-FRP-Members/blob/main/FRP_RC.png" alt="Image">
+            <img src="https://github.com/MHR-u/Shear-Capacity-of-FRP-Members/blob/main/FRP_RC.png?raw=true" alt="Image">
         </div>
     </div>
     <hr>
